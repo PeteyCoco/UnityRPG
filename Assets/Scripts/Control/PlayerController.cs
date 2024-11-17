13d14eb4b9a2269bb0ad16ce05e793de67e9ab1a
PeteyCoco <peter.carsten.collins@gmail.com>
2024-11-17 17:49:37 -0500

    Created Fighter script

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
+using RPG.Combat;
 using RPG.Movement;
+using System;
 
 namespace RPG.Control
 {
@@ -7,20 +9,46 @@ namespace RPG.Control
     {
         void Update()
         {
+            InteractWithCombat();
+            InteractWithMovement();
+        }
+
+        private void InteractWithCombat()
+        {
+            RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
+            foreach (RaycastHit hit in hits)
+            {
+                // Attack any hit combat targets
+                CombatTarget target = hit.collider.gameObject.GetComponent<CombatTarget>();
+                if (!target) continue;
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                    GetComponent<Fighter>().Attack(target);
+                }
+            }
+        }
+
+        private void InteractWithMovement()
+        {
             if (Input.GetMouseButton(0))
             {
                 MoveToCursor();
             }
         }
+
         private void MoveToCursor()
         {
-            // Move the NavMeshAgent to the cursor location
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            bool hasHit = Physics.Raycast(ray, out RaycastHit hitInfo);
+            bool hasHit = Physics.Raycast(GetMouseRay(), out RaycastHit hitInfo);
             if (hasHit)
             {
                 GetComponent<Mover>().MoveTo(hitInfo.point);
             }
+        }
+
+        private static Ray GetMouseRay()
+        {
+            return Camera.main.ScreenPointToRay(Input.mousePosition);
         }
     }
 }
