66585d78305e359ec84fbc067784168a5c88eece
PeteyCoco <peter.carsten.collins@gmail.com>
2024-11-17 17:16:48 -0500

    Created PlayerController script

---
+++ Assets/Scripts/Control/PlayerController.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            MoveToCursor();
+        }
+    }
+    private void MoveToCursor()
+    {
+        // Move the NavMeshAgent to the cursor location
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        bool hasHit = Physics.Raycast(ray, out RaycastHit hitInfo);
+        if (hasHit)
+        {
+            GetComponent<Mover>().MoveTo(hitInfo.point);
+        }
+    }
+
+}
